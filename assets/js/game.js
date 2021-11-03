591e4867b6aa79cf113904cf550616e8a9072e9f
marcos sotelo <sotelom3x@hotmail.com>
2021-11-02 17:34:48 -0700

    adding helpth and damege to robot

---
+++ assets/js/game.js
@@ -0,0 +1,25 @@
+window.prompt("What is your robot's name?");
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+playerHealth = playerHealth - enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
